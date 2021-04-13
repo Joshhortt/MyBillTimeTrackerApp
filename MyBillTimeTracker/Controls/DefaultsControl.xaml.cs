e3ad9ca4e0f071d3f1fe7898b359463f725b2c7d
Joshhortt <joshhortt@yahoo.com>
2021-04-13 11:22:10 +0100

    Create DefaultsControl. Add items and styling. Fix ClientControll. Add another row for Hourly Rate

--- MyBillTimeTracker/Controls/DefaultsControl.xaml.cs
+++ MyBillTimeTracker/Controls/DefaultsControl.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MyBillTimeTracker.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,19 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MyBillTimeTracker
+namespace MyBillTimeTracker.Controls
 {
 	/// <summary>
-	/// Interaction logic for MainWindow.xaml
+	/// Interaction logic for DefaultsControl.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class DefaultsControl : UserControl
 	{
-		public MainWindow()
+		public DefaultsControl()
 		{
 			InitializeComponent();
-			//content.Content = new MainControl();
-			content.Content = new ClientControl();
-			//content.Content = new DefaultsControl();
 		}
 	}
 }
