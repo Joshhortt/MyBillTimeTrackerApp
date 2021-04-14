47fe2c0e8dcafc02abbda23dfc2b207a74f0bbd5
Joshhortt <joshhortt@yahoo.com>
2021-04-14 11:16:10 +0100

    Add to Library a Directory 'DataAccess' with two classes: 'SqliteDataAccess', with Generic LoadData method & to class 'DataAccessHelpers an internal LoadConnectionString method.'

---
+++ MyBillTimeLibrary/DataAccess/SqliteDataAccess.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBillTimeLibrary.DataAccess
+{
+	public class SqliteDataAccess
+	{
+		// LoadData<PersonModel>("Select * from Person", null) = List<PersonModel>
+		public static List<T> LoadData<T>(string sqlStatement, Dictionary<string, object> parameters, string connectionName = "Default")
+		{
+			DynamicParameters p = new DynamicParameters();
+			parameters.ToList().ForEach(x => p.Add(x.Key, x.Value));
+			using (IDbConnection cnn = new SQLiteConnection(DataAccessHelpers.LoadConnectionString(connectionName))) 
+			{
+				var rows = cnn.Query<T>(sqlStatement, p);
+
+				return rows.ToList();
+			}
+		}
+	}
+}
